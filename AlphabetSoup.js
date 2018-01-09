1c35e0a7322357868d8be3af986793195d178f3a
pankajladhar <ladharpankaj@gmail.com>
2018-01-09 17:53:34 +0530

    Arrange word in alphabetical order

---
+++ AlphabetSoup.js
@@ -0,0 +1,12 @@
+/*
+    Using the JavaScript language, have the function AlphabetSoup(str) 
+    take the str string parameter being passed and 
+    return the string with the letters in alphabetical order (ie. hello becomes ehllo). 
+    Assume numbers and punctuation symbols will not be included in the string. 
+*/
+
+const AlphabetSoup = (str) => {
+    return str.split('').sort().join('')
+}
+
+console.log(AlphabetSoup("hello world"))
